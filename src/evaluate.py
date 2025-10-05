c239479c3400357122603fd285f0970862860a8f
flammarionick <n.eke@alustudent.com>
2025-10-05 17:56:23 +0200

    create evaluation script

---
+++ src/evaluate.py
@@ -0,0 +1,11 @@
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def evaluate_model(model, X_test, y_test, name='model'):
+    preds = model.predict(X_test)
+    print(classification_report(y_test, preds))
+    cm = confusion_matrix(y_test, preds)
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title(f'{name} Confusion Matrix')
+    plt.savefig(f'results/{name}_cm.png')
