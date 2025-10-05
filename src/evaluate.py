ba051079293c94d2f2235f805c5c084aad7f5fe5
flammarionick <n.eke@alustudent.com>
2025-10-05 17:57:39 +0200

    edit eval script

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+# Ensure results directory exists
 def evaluate_model(model, X_test, y_test, name='model'):
     preds = model.predict(X_test)
     print(classification_report(y_test, preds))
